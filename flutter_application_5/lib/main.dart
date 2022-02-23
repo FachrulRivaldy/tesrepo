6c103cc8680955d6de54bdd21794eb7f3971cafd
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2022-02-23 21:24:35 +0700

    second

--- flutter_application_5/lib/main.dart
+++ flutter_application_5/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ADADASDSAD',
       theme: ThemeData(
         // This is the theme of your application.
         //
